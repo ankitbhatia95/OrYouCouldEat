42566902773bfd96edc0e93599b2877dd9874217
Ankit Bhatia <ankit95@berkeley.edu>
2016-09-08 10:36:32 -0700

    finishes project

---
+++ foodContainers.js
@@ -0,0 +1,97 @@
+export let foodContainer = new Container({     top: 0, left: 0, right: 0, height: 60,
+		skin: new Skin({ fill: "#EAEAEA" }),
+	    contents: [	  		new Label({
+	  			name: "label0",
+	  			left: 0, right: 0, top: 0, style: new Style( { font: "12px", color: "black" } ), 				string: "Please enter a food"
+	  		}),	    ]});
+
+export let foodContainer1 = new Container({     top: 0, left: 0, right: 0, height: 60,
+		skin: new Skin({ fill: "#EAEAEA" }),
+	    contents: [	  		new Label({
+	  			name: "label0",
+	  			left: 0, right: 0, top: 0, style: new Style( { font: "12px", color: "black" } ), 				string: "Please enter a food"
+	  		}),	    ]});
+
+export let foodContainer2 = new Container({     top: 0, left: 0, right: 0, height: 60,
+		skin: new Skin({ fill: "#EAEAEA" }),
+	    contents: [	  		new Label({
+	  			name: "label0",
+	  			left: 0, right: 0, top: 0, style: new Style( { font: "12px", color: "black" } ), 				string: "Please enter a food"
+	  		}),	    ]});
+
+export let foodContainer3 = new Container({     top: 0, left: 0, right: 0, height: 60,
+		skin: new Skin({ fill: "#EAEAEA" }),
+	    contents: [	  		new Label({
+	  			name: "label0",
+	  			left: 0, right: 0, top: 0, style: new Style( { font: "12px", color: "black" } ), 				string: "Please enter a food"
+	  		}),	    ]});
+
+export let foodContainer4 = new Container({     top: 0, left: 0, right: 0, height: 60,
+		skin: new Skin({ fill: "#EAEAEA" }),
+	    contents: [	  		new Label({
+	  			name: "label0",
+	  			left: 0, right: 0, top: 0, style: new Style( { font: "12px", color: "black" } ), 				string: "Please enter a food"
+	  		}),	    ]});
+
+export let foodContainer5 = new Container({     top: 0, left: 0, right: 0, height: 60,
+		skin: new Skin({ fill: "#EAEAEA" }),
+	    contents: [	  		new Label({
+	  			name: "label0",
+	  			left: 0, right: 0, top: 0, style: new Style( { font: "12px", color: "black" } ), 				string: "Please enter a food"
+	  		}),	    ]});
+
+export let foodContainer6 = new Container({     top: 0, left: 0, right: 0, height: 60,
+		skin: new Skin({ fill: "#EAEAEA" }),
+	    contents: [	  		new Label({
+	  			name: "label0",
+	  			left: 0, right: 0, top: 0, style: new Style( { font: "12px", color: "black" } ), 				string: "Please enter a food"
+	  		}),	    ]});
+
+export let foodContainer7 = new Container({     top: 0, left: 0, right: 0, height: 60,
+		skin: new Skin({ fill: "#EAEAEA" }),
+	    contents: [	  		new Label({
+	  			name: "label0",
+	  			left: 0, right: 0, top: 0, style: new Style( { font: "12px", color: "black" } ), 				string: "Please enter a food"
+	  		}),	    ]});
+
+export let foodContainer8 = new Container({     top: 0, left: 0, right: 0, height: 60,
+		skin: new Skin({ fill: "#EAEAEA" }),
+	    contents: [	  		new Label({
+	  			name: "label0",
+	  			left: 0, right: 0, top: 0, style: new Style( { font: "12px", color: "black" } ), 				string: "Please enter a food"
+	  		}),	    ]});
+
+export let foodContainer9 = new Container({     top: 0, left: 0, right: 0, height: 60,
+		skin: new Skin({ fill: "#EAEAEA" }),
+	    contents: [	  		new Label({
+	  			name: "label0",
+	  			left: 0, right: 0, top: 0, style: new Style( { font: "12px", color: "black" } ), 				string: "Please enter a food"
+	  		}),	    ]});
+
+export let foodContainer10 = new Container({     top: 0, left: 0, right: 0, height: 60,
+		skin: new Skin({ fill: "#EAEAEA" }),
+	    contents: [	  		new Label({
+	  			name: "label0",
+	  			left: 0, right: 0, top: 0, style: new Style( { font: "12px", color: "black" } ), 				string: "Please enter a food"
+	  		}),	    ]});
+
+export let foodContainer11 = new Container({     top: 0, left: 0, right: 0, height: 60,
+		skin: new Skin({ fill: "#EAEAEA" }),
+	    contents: [	  		new Label({
+	  			name: "label0",
+	  			left: 0, right: 0, top: 0, style: new Style( { font: "12px", color: "black" } ), 				string: "Please enter a food"
+	  		}),	    ]});
+
+export let foodContainer12 = new Container({     top: 0, left: 0, right: 0, height: 60,
+		skin: new Skin({ fill: "#EAEAEA" }),
+	    contents: [	  		new Label({
+	  			name: "label0",
+	  			left: 0, right: 0, top: 0, style: new Style( { font: "12px", color: "black" } ), 				string: "Please enter a food"
+	  		}),	    ]});
+
+export let foodContainer13 = new Container({     top: 0, left: 0, right: 0, height: 60,
+		skin: new Skin({ fill: "#EAEAEA" }),
+	    contents: [	  		new Label({
+	  			name: "label0",
+	  			left: 0, right: 0, top: 0, style: new Style( { font: "12px", color: "black" } ), 				string: "Please enter a food"
+	  		}),	    ]});
